c2ab804235e7a63bc9af9ac53e1963d0abc02180
juanqueveddo <juancruzquevedo7@outlook.es>
2022-09-07 21:47:32 -0300

    primera entrega final

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// INCORPORANDO ARRAYS
+// PRIMERA ENTREGA PROYECTO FINAL
 
 const tienda = [{ "remeraNegra": 1500 }, { "remeraBlanca": 1500 }, { "buzoNegro": 4000 }, { "buzoBlanco": 4000 }, { "pantalonNegro": 6000 }, { "pantalonBlanco": 6000 }]
 
 let compraProducto = (mostrar) => {
-    alert("¡Bienvenido a Avellaneda!");
+    alert("¡Bienvenido a HOUKY!");
     let limite = parseInt(prompt("¿Cuantos productos desea elegir?"));
     // POR SI ESCRIBE STRING EN VEZ DE NUMERO
     if (isNaN(limite)) {
@@ -15,7 +15,7 @@ let compraProducto = (mostrar) => {
         if (ropa == "1") {
             console.log("El precio de la remera negra es de $" + tienda[0].remeraNegra)
         } else if (ropa == "2") {
-            console.log("El precio del remera blanca es de $" + tienda[1].remeraBlanca)
+            console.log("El precio de la remera blanca es de $" + tienda[1].remeraBlanca)
         } else if (ropa == "3") {
             console.log("El precio del buzo negro es de $" + tienda[2].buzoNegro)
         } else if (ropa == "4") {
@@ -26,7 +26,7 @@ let compraProducto = (mostrar) => {
             console.log("El precio del pantalon blanco es de $" + tienda[5].pantalonBlanco)
         }
     }
-    alert("¡Gracias por comprar en Avellaneda, hasta luego!")
+    alert("¡Gracias por comprar en Houky, hasta luego!")
 }
 
 compraProducto()
